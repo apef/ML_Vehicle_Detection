be1c6389e74e9aa50c4e3dcff6d6fd12a3b2e8a4
apef <andreas.carca2@gmail.com>
2024-05-13 16:41:54 +0200

    Refactored and added a function that sends a command and awaits a response back from the module. Is now being used to check if the device is connected to the raspberry pi.

--- Raspberry Pi/project/M5LoRaWANDriver.py
+++ Raspberry Pi/project/M5LoRaWANDriver.py
@@ -6,6 +6,45 @@ isconnected = False
 device = None
 
 
+def isModuleConnected():
+    global isconnected
+    try:
+        connectedStatus = sendCommand("AT+CGMI?\r\n", 3000)
+        if (connectedStatus):
+            print("M5LoRaWAN868 Module is connected")
+            isconnected = True
+    finally:
+        if not (isconnected):
+            print("Failed to connect to M5LoRaWAN868 Module")
+    
+
+
+
+def sendCommand(command, waitTime):
+    returnState = False
+    response = ""
+    startTime = time.time()
+    
+    
+    device.write(command.encode('utf-8'))
+    while True:
+        if (time.time() - startTime < waitTime):
+            line = device.readline().decode('utf-8').strip()
+            
+            if line == "OK":
+                returnState = True
+                break
+            if line == "FAIL":
+                returnState = False
+                break
+        else:
+            break
+    return returnState
+        
+
+    
+
+
 def writeLoRa():
     
     while True:
@@ -30,6 +69,11 @@ def run(port_, tx_pin, rx_pin, device_eui, app_eui, app_key):
 
     response = ""
     check = False 
+    
+    print("Checking if Module is connected..")
+    while not (isconnected):
+        isModuleConnected()
+    
     
     print("Starting threads..")
     readThread = threading.Thread(target=readLoRa)
